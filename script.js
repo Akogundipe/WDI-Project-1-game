5dac4d899fd014f21f163e6bc5db73ffdd914640
Akinyemi Ogundipe <ijnuthomefolder@ool-ad02062f.dyn.optonline.net>
2019-02-06 16:04:24 -0500

    Clicking on body changes color of piece dropped

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 console.log("so excited to build this game!");
-let gameboard = [];
 
-const makeNewGame = () => {
-  for (row=0; row<=5; row++) {
-  	gameboard[row] = [];
-  		for (col=0; col<=6; col++) {
-  		gameboard[row][col] = 0;
-      };
-    };
-}
-makeNewGame();
-
-gameboard =  [[0,0 ],[ 0,1 ],[ 0,2 ],[ 0,3 ],[ 0,4 ],[ 0,5 ],[ 0,6 ],[
-],[ 1,0 ],[ 1,1 ],[ 1,2 ],[ 1,3 ],[ 1,4 ],[ 1,5 ],[ 1,6 ],[
-],[ 2,0 ],[ 2,1 ],[ 2,2 ],[ 2,3 ],[ 2,4 ],[ 2,5 ],[ 2,6 ],[
-],[ 3,0 ],[ 3,1 ],[ 3,2 ],[ 3,3 ],[ 3,4 ],[ 3,5 ],[ 3,6 ],[
-],[ 4,0 ],[ 4,1 ],[ 4,2 ],[ 4,3 ],[ 4,4 ],[ 4,5 ],[ 4,6 ],[
-],[ 5,0 ],[ 5,1 ],[ 5,2 ],[ 5,3 ],[ 5,4 ],[ 5,5 ],[ 5,6 ]];
+const board = document.getElementById('container');
+let currentPlayer = 'red'
 
 //squares is a node list with all the divs of the class 'square'
 let squares = document.querySelectorAll(".square");
@@ -32,7 +17,22 @@ while (nodesArray.length) {
   twoDArray.push(tempArray);
 }
 
-
-
 squares.forEach((square, index) => square.id = "square" + index);
-//make whole board clickable (eventListener) then event.target at div i touched
+//make whole board clickable (eventListener) then event.target at div i touched event listeners
+
+board.addEventListener('click', (event) => {
+  if (event.target.className === "square") {
+    event.target.classList.add(`${currentPlayer}`);
+  }
+  if (currentPlayer === "red") {
+    currentPlayer = "black";
+  } else {
+    currentPlayer = "red";
+  }
+});
+
+for (let i = 0; i < twoDArray.length; i++) {
+  for (let j = 0; j < twoDArray[i].length; j++) {
+    twoDArray[i][j].classList[1] === twoDArray[i][j].classList[1]
+  }
+}
