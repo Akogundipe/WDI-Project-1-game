fbe26a42329a02d34d4cfb7901c6faf90f10e452
Akinyemi Ogundipe <ijnuthomefolder@ool-ad02062f.dyn.optonline.net>
2019-02-07 20:47:16 -0500

    game works perfectly now

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-console.log("so excited to build this game!");
-
 const board = document.getElementById('container');
-let currentPlayer = 'red'
+let currentPlayer = 'red';
 
 //squares is a node list with all the divs of the class 'square'
 let squares = document.querySelectorAll(".square");
@@ -20,17 +18,21 @@ while (nodesArray.length) {
 squares.forEach((square, index) => square.id = "square" + index);
 //make whole board clickable (eventListener) then event.target at div i touched event listeners
 
-board.addEventListener('click', (event) => {
+
+
+const clickFunction = (event) => {
   if (event.target.className === "square") {
     event.target.classList.add(`${currentPlayer}`);
-  }
+
     if (currentPlayer === "red") {
       currentPlayer = "black";
     } else {
       currentPlayer = "red";
     }
     checkForWin();
-  });
+    }
+  }
+  board.addEventListener('click', clickFunction);
 
 let redPlayer = document.getElementsByClassName('square red');
 let blackPlayer = document.getElementsByClassName('square black');
@@ -42,37 +44,58 @@ starter.addEventListener('click', (event) => {
   squares.forEach((square) => {
     square.classList.remove("black", "red");
   });
+  board.addEventListener('click', clickFunction);
+  currentPlayer = "red";
 });
 
 const checkForWin = () => {
   if (squares[0].className === squares[1].className && squares[0].className === squares[2].className && squares[0].className == "square black" && squares[1].className == "square black" && squares[2].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[3].className === squares[4].className && squares[3].className === squares[5].className && squares[3].className == "square black" && squares[4].className == "square black" && squares[5].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[6].className === squares[7].className && squares[6].className === squares[8].className && squares[6].className == "square black" && squares[7].className == "square black" && squares[8].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[0].className === squares[3].className && squares[0].className === squares[6].className && squares[0].className == "square black" && squares[3].className == "square black" && squares[6].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[1].className === squares[4].className && squares[1].className === squares[7].className && squares[1].className == "square black" && squares[4].className == "square black" && squares[7].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[2].className === squares[5].className && squares[2].className === squares[8].className && squares[2].className == "square black" && squares[5].className == "square black" && squares[8].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[0].className === squares[4].className && squares[0].className === squares[8].className && squares[0].className == "square black" && squares[4].className == "square black" && squares[8].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
   } else if (squares[2].className === squares[4].className && squares[2].className === squares[6].className && squares[2].className == "square black" && squares[4].className == "square black" && squares[6].className == "square black") {
-    alert("Black wins")
+    alert("Black wins");
+    board.adremoveentListener('click', clickFunction);
+  } else if (squares[0].className === squares[1].className && squares[0].className === squares[2].className && squares[0].className == "square red" && squares[1].className == "square red" && squares[2].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[3].className === squares[4].className && squares[3].className === squares[5].className && squares[3].className == "square red" && squares[4].className == "square red" && squares[5].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[6].className === squares[7].className && squares[6].className === squares[8].className && squares[6].className == "square red" && squares[7].className == "square red" && squares[8].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[0].className === squares[3].className && squares[0].className === squares[6].className && squares[0].className == "square red" && squares[3].className == "square red" && squares[6].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[1].className === squares[4].className && squares[1].className === squares[7].className && squares[1].className == "square red" && squares[4].className == "square red" && squares[7].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[2].className === squares[5].className && squares[2].className === squares[8].className && squares[2].className == "square red" && squares[5].className == "square red" && squares[8].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[0].className === squares[4].className && squares[0].className === squares[8].className && squares[0].className == "square red" && squares[4].className == "square red" && squares[8].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
+  } else if (squares[2].className === squares[4].className && squares[2].className === squares[6].className && squares[2].className == "square red" && squares[4].className == "square red" && squares[6].className == "square red") {
+    alert("Red wins");
+    board.removeEventListener('click', clickFunction);
   }
 }
-
-
-
-
-//document.querySelector('.column').children
-    //loop through the HTMLcollection
-    //write conditionals to check the row is square red or square black
-    //counter increment and current player then see if it equals 4
-
-
-
-//twoDArray[0][0].classList[0] = "square"
-//twoDArray[0][0].classList[1] = "clicked square color"
